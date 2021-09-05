25e69178b729a26f072045da7582aedfdd7ca266
Harry-McAlpine <90140326+Harry-McAlpine@users.noreply.github.com>
2021-09-05 14:43:02 +0100

    First full commit

---
+++ Assets/FrontWheelsScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontWheelsScript : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+                  transform.Rotate(2, 0, 0);
+
+        
+    }
+}
---
+++ Assets/PhysicsScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsScript : MonoBehaviour
+{
+    public Transform FLwheel;
+    public Transform FRwheel;
+    public Transform BLwheel;
+    public Transform BRwheel;
+    public Transform Body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
